e8d57210a9a97a647580b6cf8fb3850621ad732c
Peter Torelli <peter.j.torelli@gmail.com>
2024-05-19 19:06:06 -0700

    Day 25 done

---
+++ day25/part1.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+
+sub readstdin {
+    my ($graphbi, $graphuni) = @_;
+    while (<>) {
+        chomp;
+        s/: /:/;
+        s/ /:/g;
+        my ($parent, @children) = split(':');
+        foreach my $child (@children) {
+            $graphuni->{$parent}{$child} = 1;
+
+            $graphbi->{$parent}{$child} = 1;
+            $graphbi->{$child}{$parent} = 1;
+        }
+    }
+}
+
+sub coalesce {
+    my ($g) = @_;
+
+    sub submerge {
+        my ($g, $k1, $k2) = @_;
+        foreach my $c1 (keys %{$g->{$k1}}) {
+            if (defined $g->{$k2}{$c1}) {
+                foreach my $add (keys %{$g->{$k2}}) {
+                    $g->{$k1}{$add} = 1;
+                }
+                delete $g->{$k2};
+                return;
+            }
+        }
+    }
+
+    foreach my $key1 (sort keys %$g) {
+        foreach my $key2 (sort keys %$g) {
+            next if $key1 eq $key2;
+            # May have already been deleted...
+            next unless defined $g->{$key1};
+            next unless defined $g->{$key2};
+            &submerge($g, $key1, $key2);
+        }
+    }
+}
+
+sub printdotty {
+    my ($g) = @_;
+    print "digraph G {\n";
+    foreach my $k (keys %$g) {
+        my $node = $g->{$k};
+        foreach my $k2 (keys %$node) {
+            print "\t$k -> $k2;\n";
+        }
+    }
+    print "}\n";
+}
+
+sub snip {
+    my ($g, $a, $b) = @_;
+    return unless defined $g->{$a};
+    return unless defined $g->{$b};
+    delete $g->{$a}{$b};
+    delete $g->{$b}{$a};
+}
+
+sub main {
+    my %graphbi;
+    my %graphuni;
+    &readstdin(\%graphbi, \%graphuni);
+    #&printdotty(\%graphbi);
+
+    # Part 1 - Test Dataset
+
+    if (0) {
+        &snip(\%graphbi, 'hfx', 'pzl');
+        &snip(\%graphbi, 'bvb', 'cmg');
+        &snip(\%graphbi, 'nvd', 'jqt');
+    }
+
+    &coalesce(\%graphbi);
+
+    my $t = 1;
+    my $iter = 0;
+    foreach my $key (sort keys %graphbi) {
+        my $s = scalar keys %{$graphbi{$key}};
+        print "Group #$iter size = $s\n";
+        my $width = 0;
+        ++$iter;
+        print "\t";
+        foreach my $n (sort keys %{$graphbi{$key}}) {
+            print "$n ";
+            ++$width; 
+            if ($width > 19) {
+                print "\n\t";
+                $width = 0;
+            }
+        }
+        $t *= $s;
+        print "\n";
+    }
+    print "Total $t\n";
+}
+
+&main;
