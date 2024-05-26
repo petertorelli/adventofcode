339860332addbac39b477b81433e705c090eaef0
Peter Torelli <peter.j.torelli@gmail.com>
2024-05-26 09:45:07 -0700

    day 5

---
+++ 2022/day05/part1.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my @matrix;
+
+while (<>) {
+    last if /^\s*$/;
+    chomp;
+    # munge munge munge...
+    s/[\[\]]/ /g;
+    s/ /x/g;
+    s/^x//;
+    s/x$//;
+    s/x{3}/./g;
+    s/\.//g;
+    s/x/ /g;
+    my @cols = split '';
+    next if $cols[0] eq '1';
+    push @matrix, \@cols;
+}
+
+my @transpose;
+
+for (my $r=$#matrix; $r >= 0; --$r) {
+    my @row = @{$matrix[$r]};
+    for (my $c=0; $c <= $#row; ++$c) {
+        push @{$transpose[$c]}, $row[$c] unless $row[$c] eq ' ';
+    }
+}
+
+
+while (<>) {
+    next unless m/move (\d+) from (\d+) to (\d+)/;
+    my ($q, $from, $to) = ($1, $2 - 1, $3 - 1);
+    push @{$transpose[$to]}, reverse splice(@{$transpose[$from]}, -$q, $q);
+}
+
+foreach my $row (@transpose) {
+    print @$row[-1];
+}
+print "\n";
