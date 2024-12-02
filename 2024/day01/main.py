44ad1dd875b009fc0adcf48c6c79a533053459f0
Peter Torelli <peter.j.torelli@gmail.com>
2024-12-02 13:07:38 -0800

    renamed

---
+++ 2024/day01/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+
+input = np.loadtxt(sys.argv[1], dtype=int)
+
+a, b = input[:, 0], input[:, 1]
+print("Part 1:", sum(np.absolute(np.sort(a) - np.sort(b))))
+
+dictb = dict(zip(*np.unique(b, return_counts=True)))
+print("Part 2:", sum(map(lambda i: i * dictb.get(i, 0), a)))
