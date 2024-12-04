2bc314785056574e39b7df70140c3a01bba06e0c
Peter Torelli <peter.j.torelli@gmail.com>
2024-12-03 17:43:08 -0800

    day03

---
+++ 2024/day03/main.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+sub part1 {
+    my @lines = @_;
+    my $sum = 0;
+    foreach (@lines) {
+        while (/mul\((\d{1,3}),(\d{1,3})\)/gc) {
+            $sum += $1 * $2;
+        }
+    }
+    print "Part 1: $sum\n";
+}
+
+sub part2 {
+    my @lines = @_;
+    my $sum = 0;
+    my $skip = 0;
+    foreach (@lines) {
+        while (/(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))/gc) {
+            my ($opa, $opb, $opc) = ($1, $2, $3);
+            if ($opa =~ /^mul/) {
+                $sum += $opb * $opc unless $skip;
+            } elsif ($opa =~ /^don/) {
+                $skip = 1;
+            } elsif ($opa =~ /^do/) {
+                $skip = 0;
+            }
+        }
+    }
+    print "Part 2: $sum\n";
+}
+
+my @input = <>;
+&part1(@input);
+&part2(@input);
