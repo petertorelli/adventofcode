15fa0cca9dacb1617588a3a1d7b4dc85a3d860df
Peter Torelli <peter.j.torelli@gmail.com>
2024-12-08 14:56:29 -0800

    learned about list composition

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
 import numpy as np
 
@@ -9,4 +8,4 @@ a, b = input[:, 0], input[:, 1]
 print("Part 1:", sum(np.absolute(np.sort(a) - np.sort(b))))
 
 dictb = dict(zip(*np.unique(b, return_counts=True)))
-print("Part 2:", sum(map(lambda i: i * dictb.get(i, 0), a)))
+print("Part 2:", sum([i * dictb.get(i,0) for i in a ]))
