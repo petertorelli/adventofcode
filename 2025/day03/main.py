ee6c1b576e3c5f0e657b33c40c6fc62447214577
Peter Torelli <peter.j.torelli@gmail.com>
2025-12-02 22:30:00 -0800

    day03

---
+++ 2025/day03/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+def calc(bats, n):
+    total = 0
+    for i in range(0, n):
+        tail = (len(bats) - (n - i)) + 1
+        [p, mx] = max(enumerate(bats[0 : tail]), key=lambda x: x[1])
+        total += (10 ** ((n - i) - 1)) * mx
+        bats = bats[p + 1 : ]
+    return total
+
+with open(sys.argv[1], 'r') as file:
+    total1 = 0
+    total2 = 0
+    for line in file:
+        line = line.strip()
+        bats = [int(x) for x in list(line)]
+        total1 += calc(bats, 2)
+        total2 += calc(bats, 12)
+    print("Part 1:", total1)
+    print("Part 2:", total2)
